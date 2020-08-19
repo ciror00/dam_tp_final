080dcdd52c0e86fb05cc07d4b38b60cc98a6d082
Ciro Edgardo Romero <ciror00@gmail.com>
2020-08-19 19:07:34 -0300

    Refactor

--- back-end/src/routes/root.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const root: express.Router = express.Router();
-
-root.get('/', (req: express.Request, res: express.Response) => {
-    res.send('root works');
-});
-
-export default root;
